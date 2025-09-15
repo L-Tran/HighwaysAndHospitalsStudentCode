7231580ed965df3bd6a1bc298f3caa73bca5de65
L-Tran <logan.tran@menloschool.org>
2025-09-14 20:53:38 -0700

    Work log 3

--- src/HighwaysAndHospitals.java
+++ src/HighwaysAndHospitals.java
@@ -15,22 +15,27 @@ public class HighwaysAndHospitals {
      *  hospital access for all citizens in Menlo County.
      */
     public static long cost(int n, int hospitalCost, int highwayCost, int cities[][]) {
-        int highways = 0;
-        int hospitals = n;
+        // If its more cost-effective for every city to have a hospital
         if (hospitalCost > highwayCost) {
             return n * hospitalCost;
         }
         // Every City starts with a hospital
         int total = n * hospitalCost;
+        // Keep track of if a city has a hospital
+        boolean[] hasHospital = new boolean[n];
+        for (boolean b: hasHospital) {
+            b = true;
+        }
         // Keep merging cities until cannot merge anymore
         for (int i = 0; i < cities.length; i++) {
-
+            int city1 = cities[i][0];
+            int city2 = cities[i][1];
+            // If they both have a hospital connected them
+            if (hasHospital[city1] && hasHospital[city2]) {
+                total += highwayCost - hospitalCost;
+                hasHospital[city1] = false;
+            }
         }
-
-
-
-
-
-        return 0;
+        return total;
     }
 }
