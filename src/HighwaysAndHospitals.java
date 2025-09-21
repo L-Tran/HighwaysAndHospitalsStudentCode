6ed71a2f6fda4895651e050cb0e2b371926ba7e4
L-Tran <logan.tran@menloschool.org>
2025-09-21 14:12:21 -0700

    Attempted to finish Union Find + work log 4

--- src/HighwaysAndHospitals.java
+++ src/HighwaysAndHospitals.java
@@ -18,30 +18,34 @@ public class HighwaysAndHospitals {
      */
     public static long cost(int n, int hospitalCost, int highwayCost, int cities[][]) {
         // If its more cost-effective for every city to have a hospital
-        if (hospitalCost > highwayCost) {
-            return (long) n * hospitalCost;
+        if (hospitalCost < highwayCost) {
+            return ((long) n * hospitalCost);
         }
 
         int totalComponents = 0;
-        int[] components = new int[n + 1];
-        for (int i = 1; i < components.length; i++) {
-            components[i] = i;
-        }
+        int[] roots = new int[n + 1];
 
         // Find the root of each city
-        for (int i = 0; i < cities.length; i++) {
-            // Change cities root
-            int root = cities[i][0];
-            while (components[root] != root) {
-                root = components[root];
+        for (int[] highway: cities) {
+            int root = highway[0];
+            int x = highway[0];
+
+            // Find the top most root
+            while(x != 0) {
+                x = roots[x];
             }
-            int city = cities[i][1];
-            components[city] = root;
+            // Set all nodes' roots in between the original city to the topmost node
+            while(root != 0) {
+                int temp = roots[root];
+                roots[root] = x;
+                root = temp;
+
+            }
         }
 
         // Calculate number of components
-        for(int x: components) {
-            if (x == -1) {
+        for(int i = 1; i < roots.length; i++) {
+            if (roots[i] == 0) {
                 totalComponents++;
             }
         }
